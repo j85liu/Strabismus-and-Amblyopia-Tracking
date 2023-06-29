5206607a5d420f7d80b6cce2c8147c4ea8028822
j85liu <136209335+j85liu@users.noreply.github.com>
2023-06-29 15:16:47 -0500

    Add files via upload

---
+++ arduino/buttonforcamera2loop.ino
@@ -0,0 +1,148 @@
+#include <SoftwareSerial.h> 
+byte incomingbyte;
+SoftwareSerial cameraSerial = SoftwareSerial(2, 3);
+int a=0x0000,  //Read Starting address     
+    j=0,
+    k=0,
+    count=0;
+uint8_t MH,ML;
+boolean EndFlag=0;
+const int  buttonPin = 9;    // the pin that the pushbutton is attached to
+const int ledPin = 10;       // the pin that the LED is attached to
+
+// Variables will change:
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+//Send Reset command
+void SendResetCmd() {
+  cameraSerial.write((byte)0x56);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x26);
+  cameraSerial.write((byte)0x00);   
+}
+
+//Send take picture command
+void SendTakePhotoCmd() {
+  cameraSerial.write((byte)0x56);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x36);
+  cameraSerial.write((byte)0x01);
+  cameraSerial.write((byte)0x00);
+    
+  a = 0x0000; //reset so that another picture can taken
+}
+
+void FrameSize() {
+  cameraSerial.write((byte)0x56);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x34);
+  cameraSerial.write((byte)0x01);
+  cameraSerial.write((byte)0x00);  
+}
+
+//Read data
+void SendReadDataCmd() {
+  MH=a/0x100;
+  ML=a%0x100;
+      
+  cameraSerial.write((byte)0x56);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x32);
+  cameraSerial.write((byte)0x0c);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x0a);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)MH);
+  cameraSerial.write((byte)ML);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x20);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x0a);
+
+  a+=0x20; 
+}
+
+void StopTakePhotoCmd() {
+  cameraSerial.write((byte)0x56);
+  cameraSerial.write((byte)0x00);
+  cameraSerial.write((byte)0x36);
+  cameraSerial.write((byte)0x01);
+  cameraSerial.write((byte)0x03);        
+}
+
+void setup() {
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+  // initialize the LED as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize serial communication:
+  Serial.begin(38400);
+  cameraSerial.begin(38400);
+  SendResetCmd();
+  delay(1000);
+}
+
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState != lastButtonState) {
+    if (buttonState == HIGH) {
+      Serial.println("No pic");
+      digitalWrite(ledPin, HIGH);//LED ON
+    } else {
+      for(int i=0; i < 2; i++){
+      SendTakePhotoCmd();
+      digitalWrite(ledPin, LOW); //LED OFF
+      Serial.print("Start");
+  delay(100);
+  while(cameraSerial.available()>0) {
+    incomingbyte=cameraSerial.read();
+  }
+  byte b[32];
+      
+  while(!EndFlag) {  
+    j=0;
+    k=0;
+    count=0;
+    SendReadDataCmd();
+           
+    delay(75); //try going up
+    while(cameraSerial.available()>0) {
+      incomingbyte=cameraSerial.read();
+      k++;
+      if((k>5)&&(j<32)&&(!EndFlag)) {
+        b[j]=incomingbyte;
+        if((b[j-1]==0xFF)&&(b[j]==0xD9))
+        EndFlag=1;                           
+        j++;
+        count++;
+      }
+    }
+            
+    for(j=0;j<count;j++) {   
+      if(b[j]<0x10)
+        Serial.print("0");
+      Serial.print(b[j], HEX);
+    }
+    Serial.print("");//changed from serial.println();
+  }
+  
+  delay(100);
+  StopTakePhotoCmd(); //stop this picture so another one can be taken
+  EndFlag = 0; //reset flag to allow another picture to be read
+  //Serial.println();//added
+  Serial.println("End");
+  //Serial.println();
+  delay(100);
+    }
+    }
+  }
+   
+  lastButtonState = buttonState;
+}
---
+++ arduino/newlinkspiritecode.ino
@@ -0,0 +1,132 @@
+
+//Distributed as-is; no warranty is given. 
+//*********************************************************************************/
+#include <SoftwareSerial.h>
+
+/* Linksprite */
+byte incomingbyte;
+SoftwareSerial mySerial(4, 5); // RX, TX         //Configure pin 4 and 5 as soft serial port
+
+int a=0x0000,j=0,k=0,count=0;                    //Read Starting address       
+uint8_t MH,ML;
+boolean EndFlag=0;
+                               
+void SendResetCmd();
+void SendTakePhotoCmd();
+void SendReadDataCmd();
+void StopTakePhotoCmd();
+
+void setup()
+{ 
+  Serial.begin(38400);
+  //mySerial.begin(38400);
+}
+
+void loop() 
+{
+     SendResetCmd();
+     delay(4000);                               //After reset, wait 2-3 second to send take picture command
+      
+      SendTakePhotoCmd();
+
+     while(Serial.available()>0)
+      {
+        incomingbyte=Serial.read();
+
+      }   
+      byte a[32];
+      
+      while(!EndFlag)
+      {  
+         j=0;
+         k=0;
+         count=0;
+         SendReadDataCmd();
+
+         delay(25);
+          while(Serial.available()>0)
+          {
+               incomingbyte=Serial.read();
+               k++;
+               if((k>5)&&(j<32)&&(!EndFlag))
+               {
+               a[j]=incomingbyte;
+               if((a[j-1]==0xFF)&&(a[j]==0xD9))      //Check if the picture is over
+               EndFlag=1;                           
+               j++;
+         count++;
+               }
+          }
+         
+          /*for(j=0;j<count;j++)
+          {   if(a[j]<0x10)
+              Serial.print("0");
+              Serial.print(a[j],HEX);
+              Serial.print(" ");
+          }                                       //Send jpeg picture over the serial port
+          Serial.println();*/
+      }      
+     while(1);
+}
+
+//Send Reset command
+void SendResetCmd()
+{
+      Serial.write(0x56);
+      Serial.write((byte)0);
+      Serial.write(0x26);
+      Serial.write((byte)0);
+}
+
+//Send take picture command
+void SendTakePhotoCmd()
+{
+      Serial.write(0x56);
+      Serial.write((byte)0);
+      Serial.write(0x36);
+      Serial.write(0x01);
+      Serial.write((byte)0);  
+}
+
+//Read data
+void SendReadDataCmd()
+{
+      MH=a/0x100;
+      ML=a%0x100;
+      Serial.write(0x56);
+      Serial.write((byte)0);
+      Serial.write(0x32);
+      Serial.write(0x0c);
+    Serial.write((byte)0); 
+      Serial.write(0x0a);
+      Serial.write((byte)0);
+      Serial.write((byte)0);
+      Serial.write(MH);
+      Serial.write(ML);   
+      Serial.write((byte)0);
+      Serial.write((byte)0);
+      Serial.write((byte)0);
+      Serial.write(0x20);
+      Serial.write((byte)0);  
+      Serial.write(0x0a);
+      a+=0x20;                            //address increases 32£¬set according to buffer size
+}
+
+void StopTakePhotoCmd()
+{
+      Serial.write(0x56);
+      Serial.write((byte)0);
+      Serial.write(0x36);
+      Serial.write(0x01);
+      Serial.write(0x03);        
+}
+
+
+
+
+
+
+
+
+
+
